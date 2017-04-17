a9363f88d01f675db32a265649b2b1869419b8cc
davask <contact@davaskweblimited.com>
2017-04-17 12:12:37 +0200

    Automatic update 5.6-a2.4-u14.04

--- script/docker-compose.sh
+++ script/docker-compose.sh
@@ -28,7 +28,7 @@ echo "d-php-letsencrypt:
     DWL_SSLKEY_O: davask web limited - docker container
     DWL_SSLKEY_CN: davaskweblimited.com
     DWL_CERTBOT_EMAIL: docker@davaskweblimited.com
-    DWL_CERTBOT_DEBUG: false
+    DWL_CERTBOT_DEBUG: 'false'
   image: davask/d-php-letsencrypt:${branch}
   hostname: localhost
   net: bridge
