f06d540486307a7e39b62a35c37ae696a2270c3e
davask <contact@davaskweblimited.com>
2017-04-19 09:37:59 +0200

    Automatic update 5.6-a2.4-u14.04

--- script/docker-compose.sh
+++ script/docker-compose.sh
@@ -36,7 +36,7 @@ echo "d-php-letsencrypt:
   volumes:
   - ${buildDir}/etc/letsencrypt:/etc/letsencrypt
   - ${buildDir}/etc/apache2/ssl:/etc/apache2/ssl
-  - ${rootDir}/volumes/proxy/log/localhost/apache2:/var/log/apache2
+  - ${rootDir}/volumes/home/username/log/localhost/apache2:/var/log/apache2
   - ${rootDir}/volumes/home/username/http/app/sites-available:/etc/apache2/sites-available
   - ${rootDir}/volumes/home/username/files:/home/username/files
   working_dir: /var/www/html
